6781f5a7ac1695e9e916af54c3c3e1f6c7209ff3
Cristian Pallarés <cpallares@inbenta.com>
2018-02-14 11:54:26 +0100

    style: fix lint errors

--- src/index.js
+++ src/index.js
@@ -8,31 +8,33 @@ module.exports = (options = {}) => {
   });
 
   return _.flow(
-    options => _.defaultsDeep(options, {
-      srcDir: "resources/nuxt",
-      generate: {
-        dir: "storage/app/nuxt",
-      },
-      axios: {
-        baseURL: "/",
-      },
-    }),
-    options => _.merge(options, {
-      mode: "spa",
-      modules: [require.resolve("./module"), "@nuxtjs/axios"],
-      axios: {
-        proxy: process.env.LARAVEL_URL != null,
-      },
-      proxy: process.env.LARAVEL_URL
-        ? [
-            [
-              ["**/*", `!${process.env.RENDER_PATH}`],
-              {
-                target: process.env.LARAVEL_URL,
-              },
-            ],
-          ]
-        : null,
-    }),
+    options =>
+      _.defaultsDeep(options, {
+        srcDir: "resources/nuxt",
+        generate: {
+          dir: "storage/app/nuxt",
+        },
+        axios: {
+          baseURL: "/",
+        },
+      }),
+    options =>
+      _.merge(options, {
+        mode: "spa",
+        modules: [require.resolve("./module"), "@nuxtjs/axios"],
+        axios: {
+          proxy: process.env.LARAVEL_URL != null,
+        },
+        proxy: process.env.LARAVEL_URL
+          ? [
+              [
+                ["**/*", `!${process.env.RENDER_PATH}`],
+                {
+                  target: process.env.LARAVEL_URL,
+                },
+              ],
+            ]
+          : null,
+      }),
   )(options);
 };
