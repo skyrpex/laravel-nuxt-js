357f46a264994ff963062b7e7a781b04de521b1a
Cristian Pallarés <cristian@pallares.io>
2018-03-08 01:18:05 +0100

    feat(build): change output to look as the dev command

--- bin/laravel-nuxt-build.js
+++ bin/laravel-nuxt-build.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+const _ = require("lodash");
 const program = require("commander");
 const spawn = require("cross-spawn");
 const which = require("npm-which")(__dirname);
@@ -12,17 +13,21 @@ program
     "-a, --analyze",
     "Launch webpack-bundle-analyzer to optimize your bundles",
   )
+  .option("--no-color", "Disable colored output")
   .parse(process.argv);
 
 // Stop the process if the config is not OK.
 utils.validateConfig();
 
-const build = spawn.sync(
+const build = spawn(
   which.sync("nuxt"),
-  ["build", `-c=${utils.configPath}`, "--spa", program.analyze ? "-a" : ""],
-  {
-    stdio: "inherit",
-  },
+  _.filter([
+    "build",
+    `-c=${utils.configPath}`,
+    "--spa",
+    program.analyze ? "-a" : "",
+    program.color ? "--color" : null,
+  ]),
 );
-
-process.exit(build.status);
+utils.pipeStdio(build, "nuxt");
+utils.exitOnClose(build);
