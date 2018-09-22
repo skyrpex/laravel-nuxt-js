3615450985c92c1443515649e3f2419ef91fd482
Cristian Pallarés <cristian@pallares.io>
2018-09-23 01:35:15 +0200

    feat: update to nuxt v2

--- test/integration/utils.js
+++ test/integration/utils.js
@@ -1,26 +1,16 @@
-const glob = require("glob");
 const path = require("path");
-const fs = require("fs-extra");
 const spawn = require("cross-spawn");
 
 module.exports.installSelf = cwd => {
+    spawn.sync("npm", ["pack", path.resolve(__dirname, "../..")], {
+        cwd,
+    });
+
     spawn.sync(
         "npm",
-        ["install", "--no-package-lock", path.resolve(__dirname, "../..")],
+        ["install", "--no-package-lock", "laravel-nuxt-1.4.7.tgz"],
         {
             cwd,
         },
     );
-
-    const nodeModulesDir = path.resolve(__dirname, "../../node_modules");
-    glob
-        .sync("*", {
-            cwd: nodeModulesDir,
-        })
-        .forEach(packageName =>
-            fs.symlinkSync(
-                path.resolve(nodeModulesDir, packageName),
-                path.resolve(cwd, "node_modules", packageName),
-            ),
-        );
 };
